c62f62d3ddb60bd5e566eb36354a66a2153dde92
Felipe V. Rigo <felipevr@gmail.com>
2017-02-05 20:55:06 -0300

    exercicio 5 completo

---
+++ public/js/directives/minhas-diretivas.js
@@ -0,0 +1,35 @@
+angular.module('minhasDiretivas', [])
+	.directive('meuPainel', function() {
+
+		var ddo = {};
+
+		ddo.restrict = "AE";
+        ddo.transclude = true;
+
+
+		ddo.scope = {
+            titulo: '@'
+        };
+
+        ddo.templateUrl = 'js/directives/meu-painel.html';
+
+		return ddo;
+	})
+	.directive('minhaFoto', function() {
+
+		var ddo = {};
+
+		ddo.restrict = "AE";
+        ddo.transclude = true;
+
+
+		ddo.scope = {
+            titulo: '@',
+			url: '@'
+        };
+
+        ddo.templateUrl = 'js/directives/minha-foto.html';
+		//ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';   
+
+		return ddo;
+	});
