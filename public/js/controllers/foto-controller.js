0126b9c8bb87dfbbf602e1f11ecee505a4a254d2
Felipe V. Rigo <github@felipevr.eti.br>
2017-02-11 16:30:36 -0300

    exericio 7

---
+++ public/js/controllers/foto-controller.js
@@ -0,0 +1,27 @@
+// public/js/controllers/foto-controller.js
+angular.module('alurapic')
+    .controller('FotoController', function($scope, $http) {
+
+        $scope.foto = {};
+        $scope.mensagem = '';
+		
+		$scope.submeter = function() {
+            
+			if ($scope.formulario.$valid) {
+
+                $http.post('/v1/fotos', $scope.foto)
+                .success(function() {
+                    $scope.foto = {};
+                    $scope.mensagem = 'Foto cadastrada com sucesso';
+                    //console.log('Foto adicionada com sucesso');
+                })
+                .error(function(erro) {
+                    console.log(erro);
+                    $scope.mensagem = 'Não foi possível cadastrar a foto';
+                })
+            }
+			
+			
+        };
+
+    });
