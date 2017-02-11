5be9e50fc52af27b53a9137029739c80388c26f1
Felipe V. Rigo <felipevr@gmail.com>
2017-02-11 16:06:43 -0300

    algumas alterações exercicio 7

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,15 +1,16 @@
-angular.module('alurapic').controller('FotosController', function($scope, $http) {
-	
-	$scope.fotos = [];
-	$scope.filtro = '';
+angular.module('alurapic')
+	.controller('FotosController', function($scope, $http) {
+		
+		$scope.fotos = [];
+		$scope.filtro = '';
 
-	$http.get('/v1/fotos')
-	.success(function(retorno) {
-		console.log(retorno);
-		$scope.fotos = retorno; // não precisa fazer retorno.data
-	})
-	.error(function(erro) {
-		console.log(erro);
+		$http.get('/v1/fotos')
+		.success(function(retorno) {
+			console.log(retorno);
+			$scope.fotos = retorno; // não precisa fazer retorno.data
+		})
+		.error(function(erro) {
+			console.log(erro);
+		});
+
 	});
-
-});
