a5ac22b04b9273a88ec310776c0d61f5100bf2e9
Felipe V. Rigo <github@felipevr.eti.br>
2017-02-22 19:43:17 -0400

    aula 11

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,6 +1,6 @@
 // public/js/controllers/foto-controller.js
 angular.module('alurapic')
-    .controller('FotoController', function($scope, recursoFoto, $routeParams) {
+    .controller('FotoController', function($scope, recursoFoto, $routeParams, cadastroDeFotos) {
 		
         $scope.foto = {};
         $scope.mensagem = '';
@@ -19,28 +19,16 @@ angular.module('alurapic')
             
 			if ($scope.formulario.$valid) {
 
-				if($routeParams.fotoId) {
-
-					recursoFoto.update({fotoId: $scope.foto._id}, 
-                        $scope.foto, function() {
-                        $scope.mensagem = 'Foto alterada com sucesso';
-                    }, function(erro) {
-                        console.log(erro);
-                        $scope.mensagem = 'Não foi possível alterar';
-                    });
-
-				} else {
-				
-					recursoFoto.save($scope.foto, function() {
-                        $scope.foto = {};
-                        $scope.mensagem = 'Foto cadastrada com sucesso';
-                    }, function(erro) {
-                        console.log(erro);
-                        $scope.mensagem = 'Não foi possível cadastrar a foto';
-                    });
-				}
-				
-				$scope.formulario.$setPristine();
+				cadastroDeFotos.cadastrar($scope.foto)
+					.then(function(dados) {
+						$scope.mensagem = dados.mensagem;
+						if (dados.inclusao) $scope.foto = {};
+					
+						$scope.formulario.$setPristine();
+					})
+					.catch(function(erro) {
+						$scope.mensagem = erro.mensagem;
+					});
             }
 			
 			
